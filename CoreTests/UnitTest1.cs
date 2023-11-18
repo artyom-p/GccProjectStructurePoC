9902dfc73919b01476f989c24a91a948d9f7b925
Artyom Petrosov <petrosov.artyom@gmail.com>
2023-11-18 12:46:04 +0200

    Test examples

---
+++ CoreTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace CoreTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        // TODO: testing core logic
+    }
+}
---
+++ ImplementationATests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ImplementationATests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        // TODO: Implementation A tests
+    }
+}
